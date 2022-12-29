cdbf759aae350d31e3bdd01cd870e7df0c8cbb60
linarawwas <linamkraw2@gmail.com>
2022-12-29 16:19:47 +0100

    second

--- javascript/js1.js
+++ javascript/js1.js
@@ -1,2 +1,11 @@
-var a='name';
-alert(a);
+// step1: ( comment)
+
+//  (code:)
+
+// var a='name';
+// alert(a);
+
+// step2:  ( comment)
+
+var name='lina', surname='rawas', city='saida';
+alert('Name: '+name+'\nSurname: '+surname+'\nCity: '+city)
