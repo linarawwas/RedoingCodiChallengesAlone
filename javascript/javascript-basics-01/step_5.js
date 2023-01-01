1792b3d253d6270a02c6458af7d2ab3eab76fac2
linarawwas <linamkraw2@gmail.com>
2023-01-01 13:54:46 +0100

    step 5

--- javascript/javascript-basics-01/step_5.js
+++ javascript/javascript-basics-01/step_5.js
@@ -1,6 +1,6 @@
-const num1 = parseInt(prompt('Enter the 1st number: '));
-const num2 = parseInt(prompt('Enter the 2nd number: '));
-
+function multiply(){
+const num1 = document.getElementById('first_number').value;
+const num2 = document.getElementById('second_number').value;
 const multiplication = num1 * num2;
 
-alert (`The multiplication of ${num1} and ${num2} is ${multiplication}`);
+alert (`The multiplication of ${num1} and ${num2} is ${multiplication}`);}
