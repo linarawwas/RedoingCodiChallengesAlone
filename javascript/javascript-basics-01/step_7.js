58fa0aebd4bc2905d57e2bf552d3a746b7e6ee85
linarawwas <linamkraw2@gmail.com>
2023-01-01 14:02:01 +0100

    step 7

--- javascript/javascript-basics-01/step_7.js
+++ javascript/javascript-basics-01/step_7.js
@@ -1,8 +1,7 @@
 
 function getshoes(){
-    var shoe = document.getElementById("shoe_size").value;
-    var birthday = document.getElementById("year").value;
-    var result = (((((shoe*2)+5)*50)- birthday)+1766)
-    window.alert(result);
+    const shoe = document.getElementById("shoe_size").value;
+    const birthday = document.getElementById("year").value;
+    const result = (((((shoe*2)+5)*50)- birthday)+1766)
+    alert(result);
 }; 
-getshoes();
