70c19bce1c15c592b63d255e8f8786370b894ae1
linarawwas <linamkraw2@gmail.com>
2023-01-01 17:52:04 +0100

    step4

--- javascript/javascript-basics-02/step 4/step-4.js
+++ javascript/javascript-basics-02/step 4/step-4.js
@@ -1,9 +1,9 @@
-function Reset1() {
-    var yes= confirm("do you want to Clear the form");
-    if(yes){
-    document.getElementById("name").value="";
-    document.getElementById("surname").value="";
-    document.getElementById("city").value="";
+var button = document.getElementById("button")
+button.addEventListener('click', reset)
+function reset() {
+    if(confirm("do you want to Clear the form?")){
+        document.getElementById("name").value="";
+        document.getElementById("surname").value="";
+        document.getElementById("city").value="";
     }
-    
-    };
+}
