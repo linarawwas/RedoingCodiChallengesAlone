5dc5ef46a88e5a0a1d8cfca969cca5e90183951e
linarawwas <linamkraw2@gmail.com>
2023-01-01 19:09:41 +0100

    step 4

--- javascript/javascript-basics-03/step 4/step-4.js
+++ javascript/javascript-basics-03/step 4/step-4.js
@@ -1,10 +1,9 @@
+const password = document.querySelector("#password");
 const confirm = document.querySelector("#confirmation");
 const button = document.querySelector("button");
-
 const change_border = function() {
-    let password = document.querySelector("#password");
     let check = password.value;
-    if(check !== null && check !== "lina123") {
+    if(check !== null && check !== confirm.value) {
         password.style.border = "3px solid red";
         confirm.style.border = "3px solid red";
     }
