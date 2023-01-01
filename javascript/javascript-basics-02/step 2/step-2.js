1ca710c45e4f5798a713b2f7076694d2593847c8
linarawwas <linamkraw2@gmail.com>
2023-01-01 15:52:03 +0100

    step 2

--- javascript/javascript-basics-02/step 2/step-2.js
+++ javascript/javascript-basics-02/step 2/step-2.js
@@ -1,6 +1,8 @@
+// instead of adding onblur or onclick inside the html Element,
+//  you specify it here and define 
+// your function here aswell using addEventListener
+
 const name1 =document.querySelector("#name");
-name1.addEventListener('blur',(blur)=>{
-    const val =blur.target.value
-    alert("Thanks for your participation :) ");
-console.log(val);
+name1.addEventListener('blur',(thanks)=>{
+    document.getElementById("demo").innerHTML = "thank you for participating :)";
 });
