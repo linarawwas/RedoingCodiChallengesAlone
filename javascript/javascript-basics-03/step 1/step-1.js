0ba66de0af42a50a12eea6f6ce62c1897c637b05
linarawwas <linamkraw2@gmail.com>
2023-01-01 18:15:26 +0100

    step1

--- javascript/javascript-basics-03/step 1/step-1.js
+++ javascript/javascript-basics-03/step 1/step-1.js
@@ -1,9 +1,7 @@
-function borderRed(){
-    document.getElementById("image1").style.border="3px solid red";
-};
-borderRed();
-
-function borderBlack(){
-    document.getElementById("image1").style.border="3px solid black";
-};
-borderBlack();
+const img = document.getElementById("image1")
+img.addEventListener('mouseover',(borderRed)=>{
+    img.style.border="3px solid red";
+})
+img.addEventListener('mouseout',(noBorder)=>{
+    img.style.borderWidth="0px";
+})
