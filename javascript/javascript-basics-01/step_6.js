035b358362fe87d98c1ebaf4370d6b1686a8a3ac
linarawwas <linamkraw2@gmail.com>
2023-01-01 13:58:20 +0100

    step 6

--- javascript/javascript-basics-01/step_6.js
+++ javascript/javascript-basics-01/step_6.js
@@ -1,6 +1,6 @@
-const num1 = parseInt(prompt('Enter the first number '));
-const num2 = parseInt(prompt('Enter the second number '));
-
-const division = num1 / num2;
-
-alert (`the remainder of ${num1} divided by ${num2} is ${division}`);
+function remainder(){
+    const num1 = document.getElementById('first_number').value;
+    const num2 = document.getElementById('second_number').value;
+    const remainder = num1 % num2;
+    
+    alert (`The remainder of ${num1} and ${num2} is ${remainder}`);}
